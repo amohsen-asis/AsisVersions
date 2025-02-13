a40fee3cc69568968add177cfb50a4d27bc526fc
a.mohsen <a.mohsen@asis-it.com>
2025-02-13 14:03:57 +0200

    c

--- ASIS.API/obj/Debug/net8.0/ASIS.API.AssemblyInfo.cs
+++ ASIS.API/obj/Debug/net8.0/ASIS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASIS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053faa6ee38191accb809a860e7237175d977721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3872bf3a1b26b666da938e250fa3432684f2fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASIS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASIS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
